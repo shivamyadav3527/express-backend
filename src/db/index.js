f5debca547ab2c367161845901c13461e981e235
Shivam <shivamyadav3527@gmail.com>
2024-12-09 13:33:15 +0530

    add user and video model

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
+
+//This is asynchronous function thats why it is returning the promise;
 const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
